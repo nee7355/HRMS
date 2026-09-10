e20b8f36bf85217ee53f8c7a118216804aecf254
Neeraj <neekumar334@gmail.com>
2026-09-10 23:01:14 +0530

    fixed import

--- frontend/src/views/pages/auth/register/index.jsx
+++ frontend/src/views/pages/auth/register/index.jsx
@@ -8,7 +8,7 @@ import Box from '@mui/material/Box';
 // @project
 import RouterLink from '@/components/Link';
 import { SocialTypes } from '@/enum';
-import AuthRegister from '@/views/auth/register/AuthRegister';
+import AuthRegister from '@/views/pages/auth/register/AuthRegister';
 // import AuthSocial from '@/views/pages/auth/AuthSocial';
 
 /***************************  AUTH - REGISTER  ***************************/
