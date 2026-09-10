56317eae24c67abe401ae7b0d040aa9f20741c12
Neeraj <neekumar334@gmail.com>
2026-09-10 23:06:14 +0530

    allowed origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,10 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: [
+        'http://localhost:3000',
+        'https://hrms-frontend-6kmi.onrender.com'
+    ],
     credentials: true,
 }));
 
